29fed1b683305093472ddc3e36fce48f3cef5f02
nswimmer <nswimmer@physics.ucsb.edu>
2020-05-13 12:08:07 -0700

    Added comment to picturec.rules . Updated install.sh to reload udev rules and start the redis server automatically when loaded in. Created hemtduino.service still testing it out to try to get the program up and running

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 # Stuff will go here first
 
-sudo cp ../etc/redis/redis.conf /etc/redis/
-sudo cp ../etc/systemd/system/redis.service /etc/systemd/system/
-sudo cp ../etc/udev/rules.d/picturec.rules /etc/udev/rules.d/piccturec.rules
+sudo cp etc/redis/redis.conf /etc/redis/
+sudo cp etc/systemd/system/* /etc/systemd/system/
+sudo cp etc/udev/rules.d/* /etc/udev/rules.d/
+
+# Load the udev rules
+sudo udevadm control --reload-rules
+sudo udevadm trigger
+
+# Start redis server
+sudo systemctl enable /etc/systemd/system/redis.service
+sudo systemctl start redis.service
