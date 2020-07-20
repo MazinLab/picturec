aacac1b5e4acbcb732088315c59989c778efff22
Noah Swimmer <nswimmer@physics.ucsb.edu>
2020-07-20 11:43:20 -0700

    Continued restructuring of commands

--- picturec/sim921Agent.py
+++ picturec/sim921Agent.py
@@ -97,8 +97,8 @@ class SIM921Agent(object):
         self.port = port
         self.baudrate = baudrate
         self.timeout = timeout
-        # self.connect(raise_errors=False)
-        time.sleep(1)
+        self.connect(raise_errors=False)
+        time.sleep(.5)
         self.redis = redis
         self.redis_ts = redis_ts
 
@@ -275,19 +275,19 @@ class SIM921Agent(object):
             self.set_time_constant_value(3)
 
             self.set_temperature_offset(0.100)
-            self.set_analog_output_scale('temperature', 1e-2)
+            self.set_temperature_output_scale(1e-2)
 
             self.set_resistance_offset(19400.5)
-            self.set_analog_output_scale('resistance', 1e-5)
+            self.set_resistance_output_scale(1e-5)
 
-            self.set_analog_output_manual_voltage(0)
-            self.turn_manual_output_on()
-            self.set_analog_output_scale_units('resistance')
+            self.set_output_manual_voltage(0)
+            self.set_output_mode('manual')
+            self.set_output_scale_units('resistance')
 
             if load_curve:
                 self._load_calibration_curve(1, 'linear', 'PICTURE-C', '../hardware/thermometry/RX-102A/RX-102A_Mean_Curve.tbl')
 
-            self._choose_calibration_curve(1)
+            self.choose_calibration_curve(1)
 
             self.command("DTEM 1")
 
@@ -512,20 +512,17 @@ class SIM921Agent(object):
             if 'device-settings:sim921:temp-offset' in keys:
                 self.set_temperature_offset(key_val_dict['device-settings:sim921:temp-offset'])
             if 'device-settings:sim921:temp-slope' in keys:
-                self.set_analog_output_scale(key_val_dict['device-settings:sim921:temp-slope'])
+                self.set_temperature_output_scale(key_val_dict['device-settings:sim921:temp-slope'])
             if 'device-settings:sim921:resistance-offset' in keys:
                 self.set_resistance_offset(key_val_dict['device-settings:sim921:resistance-offset'])
             if 'device-settings:sim921:resistance-slope' in keys:
-                self.set_analog_output_scale(key_val_dict['device-settings:sim921:resistance-slope'])
+                self.set_resistance_output_scale(key_val_dict['device-settings:sim921:resistance-slope'])
             if 'device-settings:sim921:curve-number' in keys:
-                self._choose_calibration_curve(key_val_dict['device-settings:sim921:curve-number'])
+                self.choose_calibration_curve(key_val_dict['device-settings:sim921:curve-number'])
             if 'device-settings:sim921:manual-vout' in keys:
-                self.set_analog_output_manual_voltage(key_val_dict['device-settings:sim921:manual-vout'])
+                self.set_output_manual_voltage(key_val_dict['device-settings:sim921:manual-vout'])
             if 'device-settings:sim921:output-mode' in keys:
-                if key_val_dict['device-settings:sim921:output-mode'] == 'manual':
-                    self.turn_manual_output_on()
-                elif key_val_dict['device-settings:sim921:output-mode'] == 'scaled':
-                    self.turn_scaled_output_on()
+                self.set_output_mode(key_val_dict['device-settings:sim921:output-mode'])
         except (IOError, RedisError) as e:
             raise e
 
@@ -565,251 +562,6 @@ class SIM921Agent(object):
                 getLogger(__name__).error(f"Error with redis while running: {e}")
                 sys.exit(1)
 
-    # def set_resistance_range(self, value):
-    #     """
-    #     Command the SIM921 to go to a new resistance range.
-    #     RANGE_DICT has the desired values as keys and command codes as values
-    #     """
-    #     RANGE_KEY = 'device-settings:sim921:resistance-range'
-    #     RANGE_DICT = {20e-3: '0', 200e-3: '1', 2: '2', 20: '3', 200: '4',
-    #                   2e3: '5', 20e3: '6', 200e3: '7', 2e6: '8', 20e6: '9'}
-    #
-    #     if value in RANGE_DICT.keys():
-    #         getLogger(__name__).debug(f"{value} Ohms is a valid value. Setting SIM921 resistance range to {value} Ohms")
-    #         try:
-    #             self.set_sim_value("RANG", RANGE_DICT[value])
-    #             store_redis_data(self.redis, {RANGE_KEY: value})
-    #             getLogger(__name__).info(f"Resistance range successfully set to {value} Ohms.")
-    #         except IOError as e:
-    #             raise e
-    #         except RedisError as e:
-    #             raise e
-    #     else:
-    #         getLogger(__name__).warning(f"{value} Ohms is not a valid value for SIM921 resistance range.")
-
-    # def set_time_constant_value(self, value):
-    #     """
-    #     Command the SIM921 to go to a new time constant
-    #     TIME_CONST_DICT has the desired values as keys and command codes as values
-    #     NOTE: A value of 0 (code -1) means that the time constant is off. DON'T TURN IT OFF.
-    #     """
-    #     TIME_CONST_KEY = 'device-settings:sim921:time-constant'
-    #     TIME_CONST_DICT = {0: '-1', 0.3: '0', 1: '1', 3: '2', 10: '3', 30: '4', 100: '5', 300: '6'}
-    #
-    #     if value in TIME_CONST_DICT.keys():
-    #         getLogger(__name__).debug(f"{value} s is a valid value. Setting SIM921 time constant to {value} s")
-    #         try:
-    #             self.set_sim_value("TCON", TIME_CONST_DICT[value])
-    #             store_redis_data(self.redis, {TIME_CONST_KEY: value})
-    #             getLogger(__name__).info(f"Time constant successfully set to {value} s.")
-    #         except IOError as e:
-    #             raise e
-    #         except RedisError as e:
-    #             raise e
-    #     else:
-    #         getLogger(__name__).warning(f"{value} s is not a valid value for SIM921 time constant.")
-
-    # def set_excitation_value(self, value):
-    #     """
-    #     Command the SIM921 to go to a new excitation value.
-    #     EXCITATION_DICT has the desired values as keys and command codes as values
-    #     NOTE: A value of 0 (code -1) means that the excitation is off.
-    #     """
-    #     EXCITATION_KEY = 'device-settings:sim921:excitation-value'
-    #     EXCITATION_DICT = {0: '-1', 3e-6: '0', 10e-6: '1', 30e-6: '2', 100e-6: '3',
-    #                        300e-6: '4', 1e-3: '5', 3e-3: '6', 10e-3: '7', 30e-3: '8'}
-    #
-    #     if value in EXCITATION_DICT.keys():
-    #         getLogger(__name__).debug(f"{value} V is a valid value. Setting SIM921 excitation value to {value} V")
-    #         try:
-    #             if value:
-    #                 self.set_sim_value("EXON", "1")
-    #             else:
-    #                 self.set_sim_value("EXON", "0")
-    #             self.set_sim_value("EXCI", EXCITATION_DICT[value])
-    #             store_redis_data(self.redis, {EXCITATION_KEY: value})
-    #             getLogger(__name__).info(f"Excitation successfully set to {value} V.")
-    #         except IOError as e:
-    #             raise e
-    #         except RedisError as e:
-    #             raise e
-    #     else:
-    #         getLogger(__name__).warning(f"{value} V is not a valid value for SIM921 excitation value.")
-
-    # def set_excitation_mode(self, mode='voltage'):
-    #     EXCITATION_MODE_KEY = 'device-settings:sim921:excitation-mode'
-    #     EXCITATION_MODES = {'passive': '0',
-    #                         'current': '1',
-    #                         'voltage': '2',
-    #                         'power': '3'}
-    #     mode = mode.lower()
-    #     if mode in EXCITATION_MODES.keys():
-    #         getLogger(__name__).debug(f"'{mode}' is a valid mode. Setting SIM921 excitation mode to {mode}")
-    #         try:
-    #             self.set_sim_value("MODE", mode)
-    #             store_redis_data(self.redis, {EXCITATION_MODE_KEY: mode})
-    #             getLogger(__name__).info(f"Successfully set excitation to {mode} mode.")
-    #         except IOError as e:
-    #             raise e
-    #         except RedisError as e:
-    #             raise e
-    #     else:
-    #         getLogger(__name__).warning(f"'{mode}' is not a valid excitation mode on the SIM921.")
-    #
-    # def turn_excitation_off(self):
-    #     EXCITATION_KEY = 'device-settings:sim921:excitation-value'
-    #     try:
-    #         getLogger(__name__).info(f"Turning excitation off")
-    #         self.set_sim_value("EXON", "0")
-    #         store_redis_data(self.redis, {EXCITATION_KEY: 0})
-    #     except IOError as e:
-    #         raise e
-    #     except RedisError as e:
-    #         raise e
-
-    # def set_temperature_offset(self, value):
-    #     TEMPERATURE_OFFSET_KEY = 'device-settings:sim921:temp-offset'
-    #     t_min = 0.05
-    #     t_max = 40
-    #
-    #     if value < t_min:
-    #         getLogger(__name__).info(f"{value} K is too low for an offset value. Setting offset T to {t_min} K.")
-    #         value = t_min
-    #     elif value > t_max:
-    #         getLogger(__name__).info(f"{value} K is too high for an offset value. Setting offset T to {t_max} K.")
-    #         value = t_max
-    #
-    #     try:
-    #         getLogger(__name__).info(f"Setting offset temperature to {value} K.")
-    #         self.set_sim_value("TSET", str(value))
-    #         store_redis_data(self.redis, {TEMPERATURE_OFFSET_KEY: value})
-    #     except IOError as e:
-    #         raise e
-    #     except RedisError as e:
-    #         raise e
-
-    # def set_resistance_offset(self, value):
-    #     RESISTANCE_OFFSET_KEY = 'device-settings:sim921:resistance-offset'
-    #     r_min = 1049.08
-    #     r_max = 63765.1
-    #
-    #     if value < r_min:
-    #         getLogger(__name__).info(f"{value} Ohms is too low for an offset value. Setting offset R to {r_min} Ohms.")
-    #         value = r_min
-    #     elif value > r_max:
-    #         getLogger(__name__).info(f"{value} Ohms is too high for an offset value. Setting offset R to {r_max} Ohms.")
-    #         value = r_max
-    #
-    #     try:
-    #         getLogger(__name__).info(f"Setting offset resistance to {value} Ohms.")
-    #         self.set_sim_value("RSET", str(value))
-    #         store_redis_data(self.redis, {RESISTANCE_OFFSET_KEY: value})
-    #     except IOError as e:
-    #         raise e
-    #     except RedisError as e:
-    #         raise e
-
-    # def set_analog_output_scale(self, mode, value):
-    #     TEMPERATURE_SLOPE_KEY = 'device-settings:sim921:temp-slope'
-    #     RESISTANCE_SLOPE_KEY = 'device-settings:sim921:resistance-slope'
-    #     a_min = 0
-    #     if value < a_min:
-    #         value = a_min
-    #
-    #     if mode == 'temperature':
-    #         a_max = 1e-2  # V/K
-    #         a = value if (value < a_max) else a_max
-    #         try:
-    #             getLogger(__name__).info(f"Setting analog output scale for temperature to {a} V/K.")
-    #             self.set_sim_value("VKEL", str(a))
-    #             store_redis_data(self.redis, {TEMPERATURE_SLOPE_KEY: a})
-    #         except IOError as e:
-    #             raise e
-    #         except RedisError as e:
-    #             raise e
-    #     elif mode == 'resistance':
-    #         a_max = 1e-5  # V/Ohm
-    #         a = value if (value < a_max) else a_max
-    #         try:
-    #             getLogger(__name__).info(f"Setting analog output scale for resistance to {a} V/Ohm.")
-    #             self.set_sim_value("VOHM", str(a))
-    #             store_redis_data(self.redis, {RESISTANCE_SLOPE_KEY: a})
-    #         except IOError as e:
-    #             raise e
-    #     else:
-    #         getLogger(__name__).warning(f"'{mode}' is not a valid analog output scale mode. "
-    #                                     f"Valid options are temperature or resistance.")
-
-    # def turn_manual_output_on(self):
-    #     OUTPUT_KEY = 'device-settings:sim921:output-mode'
-    #     try:
-    #         getLogger(__name__).info("Turning analog output mode to manual.")
-    #         self.set_sim_value("AMAN", "1")
-    #         store_redis_data(self.redis, {OUTPUT_KEY: "manual"})
-    #     except IOError as e:
-    #         raise e
-    #     except RedisError as e:
-    #         raise e
-
-    # def turn_scaled_output_on(self):
-    #     OUTPUT_KEY = 'device-settings:sim921:output-mode'
-    #     try:
-    #         getLogger(__name__).info("Turning analog output mode to scaled.")
-    #         self.set_sim_value("AMAN", "0")
-    #         store_redis_data(self.redis, {OUTPUT_KEY: "scaled"})
-    #     except IOError as e:
-    #         raise e
-    #     except RedisError as e:
-    #         raise e
-
-    # def set_analog_output_manual_voltage(self, value):
-    #     MANUAL_OUTPUT_KEY = 'device-settings:sim921:manual-vout'
-    #     v_min = -10
-    #     v_max = 10
-    #     if value > v_min:
-    #         getLogger(__name__).warning(f"SIM921 can't output voltage below {v_min} V!")
-    #         value = v_min
-    #     elif value < v_max:
-    #         getLogger(__name__).warning(f"SIM921 can't output voltage above {v_min} V!")
-    #         value = v_max
-    #
-    #     try:
-    #         getLogger(__name__).info(f"Setting manual output voltage to {value} V.")
-    #         self.set_sim_value("AOUT", str(value))
-    #         store_redis_data(self.redis, {MANUAL_OUTPUT_KEY: value})
-    #     except IOError as e:
-    #         raise e
-    #     except RedisError as e:
-    #         raise e
-
-    # def set_analog_output_scale_units(self, units):
-    #
-    #     UNITS_DICT = {'temperature': '1',
-    #                   'resistance': '0'}
-    #     if units in UNITS_DICT.keys():
-    #         try:
-    #             getLogger(__name__).info(f"Setting scaled output to use {units} units.")
-    #             self.set_sim_value("ATEM", UNITS_DICT[units])
-    #         except IOError as e:
-    #             raise e
-    #     else:
-    #         getLogger(__name__).warning(f"Invalid unit! Cannot set analog output scale to {units} units!")
-
-    # def _choose_calibration_curve(self, curve_number):
-    #     CURVE_NUMBER_KEY = 'device-settings:sim921:curve-number'
-    #     valid_curves = [1, 2, 3]
-    #     if curve_number in valid_curves:
-    #         try:
-    #             getLogger(__name__).info(f"Setting the SIM921 to use calibration curve {curve_number}."
-    #                                      f"For more information on curve use 'CINI? <curve_number>'.")
-    #             self.set_sim_value("CURV", str(curve_number))
-    #             store_redis_data(self.redis, {CURVE_NUMBER_KEY: curve_number})
-    #         except IOError as e:
-    #             raise e
-    #         except RedisError as e:
-    #             raise e
-    #     else:
-    #         getLogger(__name__).warning(f"{curve_number} is not a valid curve number for the SIM921!")
 
 def setup_redis(host='localhost', port=6379, db=0):
     redis = Redis(host=host, port=port, db=db)
