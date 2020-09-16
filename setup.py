b83dcdce0cdebab7dcc236701ec0813dd0505db2
Noah Swimmer <nswimmer@physics.ucsb.edu>
2020-09-16 09:48:42 -0700

    modified setup to account for the actual revision that we're working on.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='picturec',
-    version='0.1',
+    version='0.3.0',
     author='Noah Swimmer',
     author_email='nswimmer@ucsb.edu',
     description='PICTURE-C Control Software',
