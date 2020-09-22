15d5081ef75427a1b7c8d01b1bbad14a65b95cf0
Noah Swimmer <nswimmer@physics.ucsb.edu>
2020-09-22 12:47:11 -0700

    implementing a new pubsub listen with a reconnection for testing

--- picturec/pcRedis.py
+++ picturec/pcRedis.py
@@ -76,38 +76,96 @@ class PCRedis(object):
         vals = [self.redis.get(k).decode("utf-8") for k in keys]
         return vals if not return_dict else {k: v for k, v in zip(keys, vals)}
 
-    def pubsub_listen(self, ps_keys: list, message_handler, status_key=None, loop_interval=0.01):
-        logging.getLogger(__name__).info(f"Subscribing redis to {ps_keys}")
-        ps = self.redis.pubsub()
-        [ps.subscribe(key) for key in ps_keys]
-        logging.getLogger(__name__).info(f"Channels are {ps.channels}")
+    def pubsub_subscribe(self, keys, ps=None):
+        logging.getLogger(__name__).info(f"Subscribing redis to {keys}")
+        try:
+            logging.getLogger(__name__).debug(f"Attempting to create redis pubsub object")
+            ps = self.redis.pubsub()
+            [ps.subscribe(key) for key in keys]
+            logging.getLogger(__name__).info(f"Subcribed to: {ps.channels}")
+        except RedisError as e:
+            logging.getLogger(__name__).critical(f"Redis Error: {e}")
+            return None
+        except ConnectionError as e:
+            logging.getLogger(__name__).critical(f"Connection Error: {e}")
+            return None
 
+        return ps
+
+    def pubsub_unsubscribe(self, pubsub_object):
+        try:
+            pubsub_object.unsubscribe()
+            return None
+        except RedisError as e:
+            logging.getLogger(__name__).critical(f"Redis Error in pubsub unsubscribe: {e}")
+            sys.exit()
+        except ConnectionError as e:
+            logging.getLogger(__name__).critical(f"Connection Error in pubsub unsubscribe: {e}")
+            sys.exit()
+
+
+    def pubsub_listen(self, ps_keys: list, message_handler, status_key=None, loop_interval=0.001):
+        subbed = False
+
+        counter = 0
         while True:
+            while not subbed:
+                ps = self.pubsub_subscribe(ps_keys)
+                if ps is not None:
+                    subbed = True
+            counter += 1
             try:
+                if (counter%100) == 0:
+                    logging.getLogger(__name__).debug(f"{counter}: {time.time()}")
                 msg = ps.get_message()
                 if msg:
-                    if msg['type'] == 'message':
-                        logging.getLogger(__name__).info(f"Redis pubsub client received a message: {msg}")
-                        message_handler(msg)
-                    elif msg['type'] == 'subscribe':
-                        logging.getLogger(__name__).debug(f"Redis subscribed to {msg['channel']}")
-                    else:
-                        logging.getLogger(__name__).debug(f"Redis received a message of unknown type: {msg}")
-            except (RedisError, ConnectionError) as e:  # TODO: There's not a lot of documentation on why this occurs,
-                # but sometimes redis just kicks you out. so figure out how to either disable timeouts or reconnect well
-                logging.getLogger(__name__).warning(f"Exception in pubsub operation has occurred: {e}")
-                ps = None
-                time.sleep(.1)
-                ps = self.redis.pubsub()
-                [ps.subscribe(key) for key in ps_keys]
-                logging.getLogger(__name__).debug(f"Resubscribed to {ps.channels}")
-                # logging.getLogger(__name__).critical(f"Redis error: {e}")
-                # sys.exit(1)
+                    logging.getLogger(__name__).debug(f"Pubsub client received a message!")
+                    message_handler(msg)
+            except RedisError as e:
+                logging.getLogger(__name__).critical(f"Redis Error in listening: {e}")
+                ps = self.pubsub_unsubscribe(ps)
+                subbed = False
+            except ConnectionError as e:
+                logging.getLogger(__name__).critical(f"Connection Error while listening: {e}")
+                ps = self.pubsub_unsubscribe(ps)
+                subbed = False
             except IOError as e:
                 logging.getLogger(__name__).error(f"Error: {e}")
-                if status_key:
-                    self.store({status_key: f"Error: {e}"})
             time.sleep(loop_interval)
+
+        # logging.getLogger(__name__).info(f"Subscribing redis to {ps_keys}")
+        # ps = self.redis.pubsub()
+        # [ps.subscribe(key) for key in ps_keys]
+        # logging.getLogger(__name__).info(f"Channels are {ps.channels}")
+        #
+        # ps.listen()
+        #
+        # while True:
+        #     try:
+        #         msg = ps.get_message()
+        #         if msg:
+        #             if msg['type'] == 'message':
+        #                 logging.getLogger(__name__).info(f"Redis pubsub client received a message: {msg}")
+        #                 message_handler(msg)
+        #             elif msg['type'] == 'subscribe':
+        #                 logging.getLogger(__name__).debug(f"Redis subscribed to {msg['channel']}")
+        #             else:
+        #                 logging.getLogger(__name__).debug(f"Redis received a message of unknown type: {msg}")
+        #     except (RedisError, ConnectionError) as e:  # TODO: There's not a lot of documentation on why this occurs,
+        #         # but sometimes redis just kicks you out. so figure out how to either disable timeouts or reconnect well
+        #         logging.getLogger(__name__).warning(f"Exception in pubsub operation has occurred: {e}")
+        #         ps = None
+        #         time.sleep(.1)
+        #         ps = self.redis.pubsub()
+        #         [ps.subscribe(key) for key in ps_keys]
+        #         logging.getLogger(__name__).debug(f"Resubscribed to {ps.channels}")
+        #         # logging.getLogger(__name__).critical(f"Redis error: {e}")
+        #         # sys.exit(1)
+        #     except IOError as e:
+        #         logging.getLogger(__name__).error(f"Error: {e}")
+        #         if status_key:
+        #             self.store({status_key: f"Error: {e}"})
+        #     time.sleep(loop_interval)
 
     def handler(self, message):
         """
