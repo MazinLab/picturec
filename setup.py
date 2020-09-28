ea73586c1f7a420d202fa12bba9911b86ba33a82
Noah Swimmer <nswimmer@physics.ucsb.edu>
2020-09-28 15:41:39 -0700

    updated redis functionality. Pubsub should work as an indefinite 'listener' now.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='picturec',
-    version='0.3.2',
+    version='0.3.3',
     author='Noah Swimmer',
     author_email='nswimmer@ucsb.edu',
     description='PICTURE-C Control Software',
