3d6efffa3d0fa07a104f8a3179b5c8712fd9b874
Noah Swimmer <nswimmer@physics.ucsb.edu>
2020-10-07 09:30:39 -0700

    A new review with Jeb, with pubsub working and implemented as a first pass with currentduino

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='picturec',
-    version='0.3.3',
+    version='0.3.4',
     author='Noah Swimmer',
     author_email='nswimmer@ucsb.edu',
     description='PICTURE-C Control Software',
