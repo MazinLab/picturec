53aab60c7d3b09e0e6a42a54f5540c08a6fe941a
Noah Swimmer <nswimmer@physics.ucsb.edu>
2020-10-07 09:54:12 -0700

    forgot to change a function name

--- picturec/currentduinoAgent.py
+++ picturec/currentduinoAgent.py
@@ -171,7 +171,7 @@ if __name__ == "__main__":
     pollthread.daemon = True
     pollthread.start()
 
-    heatswitchthread = threading.Thread(target=redis.pubsub_listen, name='Command Monitoring Thread',
+    heatswitchthread = threading.Thread(target=redis.ps_listen, name='Command Monitoring Thread',
                                         args=([HEATSWITCH_MOVE_KEY], handle_redis_message, STATUS_KEY, LOOP_INTERVAL))
     heatswitchthread.daemon = True
     heatswitchthread.start()
