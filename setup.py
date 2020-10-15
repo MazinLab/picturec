898cfdcfbe5243f5b7589b904fbb412efe802161
Noah Swimmer <nswimmer@physics.ucsb.edu>
2020-10-15 11:28:40 -0700

    trying to keep up with tagging new versions/updates.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='picturec',
-    version='0.3.5',
+    version='0.3.6',
     author='Noah Swimmer',
     author_email='nswimmer@ucsb.edu',
     description='PICTURE-C Control Software',
