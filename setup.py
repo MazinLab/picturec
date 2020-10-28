663ae055c6e9fd52c346fcabde056d4af34a6395
Noah Swimmer <nswimmer@physics.ucsb.edu>
2020-10-28 15:09:46 -0700

    with sim921 being mostly completed (save an engineering function), updated from 0.3.6 to 0.4.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='picturec',
-    version='0.3.6',
+    version='0.4.0',
     author='Noah Swimmer',
     author_email='nswimmer@ucsb.edu',
     description='PICTURE-C Control Software',
