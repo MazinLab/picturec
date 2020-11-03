7058e52f5eff17f51c7dac18bc7c1ecd476d164e
Noah Swimmer <nswimmer@physics.ucsb.edu>
2020-11-03 12:10:28 -0800

    Reformatted hemtduino to use pcredis and serialAgent

--- picturec/hemttempAgent.py
+++ picturec/hemttempAgent.py
@@ -35,148 +35,91 @@ KEY_DICT = {msg_idx: key for (msg_idx, key) in zip(np.arange(0, 15, 1), KEYS)}
 STATUS_KEY = "status:device:hemtduino:status"
 FIRMWARE_KEY = "status:device:hemtduino:firmware"
 
-class Hemtduino(object):
-    def __init__(self, port, baudrate=115200, timeout=0.1):
-        self.ser = None
-        self.port = port
-        self.baudrate = baudrate
-        self.timeout = timeout
-        self.connect(raise_errors=False)
 
-    def connect(self, reconnect=False, raise_errors=True):
-        if reconnect:
-            self.disconnect()
+log = logging.getLogger(__name__)
 
-        try:
-            if self.ser.isOpen():
-                return
-        except Exception:
-            pass
 
-        getLogger(__name__).debug(f"Connecting to {self.port} at {self.baudrate}")
-        try:
-            self.ser = serial.Serial(port=self.port, baudrate=self.baudrate, timeout=self.timeout)
-            time.sleep(.2)
-            getLogger(__name__).debug(f"port {self.port} connection established")
-            return True
-        except (SerialException, IOError) as e:
-            self.ser = None
-            getLogger(__name__).error(f"Connecting to port {self.port} failed: {e}", exc_info=True)
-            if raise_errors:
-                raise e
-            else:
-                return False
-
-    def disconnect(self):
-        try:
-            self.ser.close()
-            self.ser = None
-        except Exception as e:
-            getLogger(__name__).info(f"Exception durring disconnect: {e}")
-
-    def send(self, msg: str, connect=True):
+class Hemtduino(agent.SerialAgent):
+    VALID_FIRMWARES = [0.0, 0.1]
+    def __init__(self, port, baudrate=115200, timeout=0.1, connect=True):
+        super().__init__(port, baudrate, timeout, name='hemtduino')
         if connect:
-            self.connect()
-        try:
-            getLogger(__name__).debug(f"Writing message: {msg}")
-            self.ser.write(msg.encode("utf-8"))
-            getLogger(__name__).debug(f"Sent {msg} successfully")
-        except (SerialException, IOError) as e:
-            self.disconnect()
-            getLogger(__name__).error(f"Send failed: {e}")
-            raise e
+            self.connect(raise_errors=False, post_connect_sleep=2)
 
-    def receive(self):
-        try:
-            data = self.ser.readline().decode("utf-8").strip()
-            getLogger(__name__).debug(f"read {data} from arduino")
-            if data[-1] != '?':
-                raise IOError('Protocol violation')
-            return data
-        except (IOError, SerialException) as e:
-            self.disconnect()
-            getLogger(__name__).debug(f"Send failed: {e}")
-            raise e
+        self.terminator = ''
 
-    def parse(self, response):
-        if response[-1] == '?':
-            response = response[:-2]
+    def format_msg(self, msg:str):
+        return f"{msg.strip().lower()}{self.terminator}"
+
+    def firmware_ok(self):
+        return self.firmware in self.VALID_FIRMWARES
+
+    @property
+    def firmware(self):
+        """ Return the firmware string or raise IOError"""
         try:
-            values = list(map(float, response.strip().split(' ')))
-            pvals = [val * (5/1023) if i % 3 else 2 * ((val * (5/1023)) - 2.5) for i, val in enumerate(values)]
-            ret = {key: v for key, v in zip(KEYS, pvals)}
+            log.debug(f"Querying currentduino firmware")
+            response = self.query("v", connect=True)
+            version, _, v = response.partition(" ")  # Arduino resonse format is "{response} {query char}"
+            version = float(version)
+            if v != "v":
+                raise ValueError('Bad format')
+            return version
+        except IOError as e:
+            log.error(f"Serial error: {e}")
+            raise e
+        except ValueError:
+            log.error(f"Bad firmware format: '{response}'")
+            raise IOError(f'Bad firmware response: "{response}"')
+
+    def read_hemt_data(self):
+        response = self.query('?', connect=True)
+        try:
+            resp = response.split(' ')
+            values = list(map(float, resp[:-1]))
+            confirm = resp[-1]
+            if confirm == '?':
+                log.info("HEMT values successfully queried")
+                pvals = [v * (5.0 / 1023.0) if i % 3 else 2 * ((v * (5.0 / 1023.0)) - 2.5) for i, v in enumerate(values)]
+                ret = {key: v for key, v in zip(KEYS, pvals)}
+            else:
+                raise ValueError(f"Nonsense was returned: {response}")
         except Exception as e:
             raise ValueError(f"Error parsing response data: {response}")
         return ret
 
-    def get_hemt_data(self):
-        try:
-            self.send('?', connect=True)
-            response = self.receive()
-            data = self.parse(response)
-        except Exception as e:
-            raise IOError(e)
-
-        return data
-
-
-def setup_redis_ts(host='localhost', port=6379, db=0):
-    redis_ts = Client(host=host, port=port, db=db)
-
-    for key in KEYS:
-        try:
-            redis_ts.create(key)
-        except RedisError:
-            getLogger(__name__).debug(f"KEY '{key}' already exists")
-            pass
-
-    return redis_ts
-
-
-def setup_redis(host='localhost', port=6379, db=0):
-    redis = Redis(host=host, port=port, db=db)
-    return redis
-
-
-def store_status(redis, status):
-    redis.set(STATUS_KEY, status)
-
-
-def store_firmware(redis, hemtduino_version):
-    redis.set(FIRMWARE_KEY, hemtduino_version)
-
-
-def store_hemt_data(redis_ts, data):
-    for k, v in data.items():
-        redis_ts.add(key=k, value=v, timestamp='*')
-
 
 if __name__ == "__main__":
 
-    logging.basicConfig()
-    log = logging.getLogger(__name__)
-    log.setLevel(logging.DEBUG)
+    logging.basicConfig(level=logging.DEBUG)
 
-    hemtduino = Hemtduino(port="/dev/hemtduino", baudrate=115200)
-    hemtduino.connect()
-    redis_ts = setup_redis_ts(host='localhost', port=6379, db=REDIS_DB)
-    redis = setup_redis(host='localhost', port=6379, db=REDIS_DB)
+    redis = PCRedis(host='127.0.0.1', port=6379, db=REDIS_DB, create_ts_keys=KEYS)
+    hemtduino = Hemtduino(port="/dev/hemtduino", baudrate=115200, timeout=0.1)
 
-    # Add grabbing firmware value here (or in connect function whenever we connect?)
-
-    store_firmware(redis)
-    time.sleep(1)
+    try:
+        firmware = hemtduino.firmware
+        redis.store({FIRMWARE_KEY: firmware})
+        if not hemtduino.firmware_ok():
+            redis.store({STATUS_KEY: 'Unsupported firmware'})
+            sys.exit(1)
+    except IOError:
+        redis.store({FIRMWARE_KEY: ''})
+        redis.store({STATUS_KEY: 'FAILURE to poll firmware'})
+        sys.exit(1)
+    except RedisError as e:
+        log.critical(f"Redis server error! {e}")
+        sys.exit(1)
 
     while True:
         try:
-            data = hemtduino.get_hemt_data()
-            store_hemt_data(redis_ts, data)
-            store_status(redis, 'OK')
+            data = hemtduino.read_hemt_data()
+            redis.store(data, timeseries=True)
+            redis.store({STATUS_KEY: 'OK'})
         except RedisError as e:
             log.error(f"Redis error {e}")
             sys.exit(1)
         except IOError as e:
             log.error(f"Error {e}")
-            store_status(redis, f"Error {e}")
+            redis.store({STATUS_KEY: f"Error: {e}"})
 
         time.sleep(QUERY_INTERVAL)
