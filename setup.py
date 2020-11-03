174cbfe71bc908758479355a22d390bd805843f6
Noah Swimmer <nswimmer@physics.ucsb.edu>
2020-11-03 12:18:26 -0800

    v0.4.1 - Hemtduino, SIM960, SIM921 ready for review

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='picturec',
-    version='0.4.0',
+    version='0.4.1',
     author='Noah Swimmer',
     author_email='nswimmer@ucsb.edu',
     description='PICTURE-C Control Software',
