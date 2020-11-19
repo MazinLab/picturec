f96990e19cb2f9a4dc7514bd7e5d0f85fdc8d332
Noah Swimmer <nswimmer@physics.ucsb.edu>
2020-11-19 13:50:43 -0800

    1 more device's agent tested

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='picturec',
-    version='0.5.2',
+    version='0.5.3',
     author='Noah Swimmer',
     author_email='nswimmer@ucsb.edu',
     description='PICTURE-C Control Software',
