4c45844b4fde1c9ea5dedc6f92a96400bb47268f
Noah Swimmer <nswimmer@physics.ucsb.edu>
2020-11-19 10:50:19 -0800

    0.5.0 -> 0.5.2 (I missed 0.5.1 because I didn't update this after testing the hemttempAgent)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='picturec',
-    version='0.5.0',
+    version='0.5.2',
     author='Noah Swimmer',
     author_email='nswimmer@ucsb.edu',
     description='PICTURE-C Control Software',
