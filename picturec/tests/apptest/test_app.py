102a9b65a0bc0b0e489a753071fb10c952e3e07e
Noah Swimmer <nswimmer@physics.ucsb.edu>
2021-04-13 13:51:09 -0700

    Added small test app

---
+++ picturec/tests/apptest/test_app.py
@@ -0,0 +1,40 @@
+import flask
+from flask_wtf import FlaskForm
+from flask_bootstrap import Bootstrap
+from flask import request, redirect, url_for, render_template, jsonify, Response
+import time
+from redis import Redis
+
+app = flask.Flask(__name__)
+app.logger.setLevel('DEBUG')
+app.secret_key = 'heres-a-big-many-charaacter-secret-key'
+bootstrap = Bootstrap(app)
+red = Redis(host='localhost', port=6379, db=0)
+
+
+@app.route('/listen')
+def listen():
+    app.logger.debug(f"listening!!")
+    return Response(foobar(), mimetype='text/event-stream', content_type='text/event-stream')
+
+def foobar():
+    pubsub = red.pubsub()
+    pubsub.subscribe('chat')
+    for message in ({'type':'message', 'data':f"{i}".encode('utf-8')} for i in range(11)):
+        if message['type'] == 'message':
+            msg = f"retry:5\ndata: {message['data'].decode('utf-8')}\n\n"
+            app.logger.debug(msg)
+            time.sleep(2)
+            yield msg
+    app.logger.info(f"pubsub ended!")
+
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/main', methods=['GET', 'POST'])
+def index():
+    form = FlaskForm()
+    return render_template('index.html', title='Home', form=form)
+
+
+if __name__ == "__main__":
+    app.run(port=8000, threaded=True, debug=True)
